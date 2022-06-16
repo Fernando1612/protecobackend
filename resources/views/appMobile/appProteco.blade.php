92c820dd38587b52f60660021757c96c876dbe45
jassiel2911 <brian29_11@comunidad.unam.mx>
2022-06-15 20:40:38 -0500

    Cambio de estilos tablas pt1

--- resources/views/appMobile/appProteco.blade.php
+++ resources/views/appMobile/appProteco.blade.php
@@ -1,58 +1,53 @@
 @extends('layouts.becarios')
 
 @section('content')
-<style>
-    table {
-  width: 100%;
-  table-layout: fixed;
-}
 
-td {
-  width:65%;
-  border:1px solid black;
-}
-    .container {
-      max-width: 450px;
-    }
-    .push-top {
-      margin-top: 50px;
-    }
-</style>
-                <table class="table">
-                  <thead>
-                      <tr class="table-warning">
-                        <td>Titulo</td>
-                        <td>Descripción</td>
-                        <td>Fecha</td>
-                        <td>Link de imagen</td>
-                        <td class="text-center">Action</td>
-                      </tr>
-                  </thead>
-            <tbody>
-            @foreach ($talleres as $taller)
-                <!-- Post preview-->
-                <div class="post-preview">
-                      <tr>
-                        <td>{{ $taller->title }}</td>
-                        <td>{{ $taller->description }}</td>
-                        <td>{{ $taller->date }}</td>
-                        <!-- Se pone la palabra "Link" porque las url son muy grandes y se salen de su cuadro -->
-                        <td><a href="{{ $taller->liga }}"> Link </a></td>
-                        <td class="text-center">
-                            <a href="{{ route('talleres.edit', $taller->id)}}" class="btn btn-primary btn-sm">Editar</a>
-                            <form action="{{ route('talleres.destroy', $taller->id)}}" method="post" style="display: inline-block">
-                              @csrf
-                              @method('DELETE')
-                              <button class="btn btn-danger btn-sm" type="submit">Delete</button>
-                            </form>
-                        </td>
-                </div>
-                <!-- Divider-->
-                <hr class="my-4" />
-                </td>
-                </tr>
-                @endforeach
-                <a class="btn btn-success" href="{{ route('talleres.create')}}"> Crear Nuevo Taller</a>
-                </tbody>
-            </table>
-            @endsection
+<div class="container">
+  <section class="container mt-5">
+    <table class="table table-striped" aria-label>
+      <thead>
+        <tr>
+          <th scope="col">Titulo</th>
+          <th scope="col">Descripción</th>
+          <th scope="col">Fecha</th>
+          <th scope="col">Link de imagen</th>
+          <th scope="col">Acción</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($talleres as $taller)
+        <!-- Post preview-->
+        <div class="post-preview">
+          <tr>
+            <td>{{ $taller->title }}</td>
+            <td>{{ $taller->description }}</td>
+            <td>{{ $taller->date }}</td>
+            <!-- Se pone la palabra "Link" porque las url son muy grandes y se salen de su cuadro -->
+            <td><a href="{{ $taller->liga }}"> Link </a></td>
+            <td class="text-center">
+              <div class="container-sm">
+                <a href="{{ route('talleres.edit', $taller->id)}}" class="btn btn-primary btn-sm">Editar</a>
+              </div>
+              <br>
+              <div class="container-sm">
+                <form action="{{ route('talleres.destroy', $taller->id)}}" method="post" style="display: inline-block">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger btn-sm" type="submit">Borrar</button>
+                </form>
+              </div>
+            </td>
+        </div>
+        <!-- Divider-->
+        <hr class="my-4" />
+        </td>
+        </tr>
+        @endforeach
+        <div class="container-sm">
+          <a class="btn btn-success" href="{{ route('talleres.create')}}"> Crear nuevo taller</a>
+        </div>
+      </tbody>
+    </table>
+  </section>
+</div>
+@endsection
--- resources/views/appMobile/mostrarCurso.blade.php
+++ resources/views/appMobile/mostrarCurso.blade.php
@@ -1,58 +1,55 @@
 @extends('layouts.becarios')
 
 @section('content')
-<style>
-    table {
-  width: 100%;
-  table-layout: fixed;
-}
 
-td {
-  width:65%;
-  border:1px solid black;
-}
-    .container {
-      max-width: 450px;
-    }
-    .push-top {
-      margin-top: 50px;
-    }
-</style>
-                <table class="table">
-                  <thead>
-                      <tr class="table-warning">
-                        <td>Titulo</td>
-                        <td>Descripción</td>
-                        <td>Fecha</td>
-                        <td>Link de imagen</td>
-                        <td class="text-center">Action</td>
-                      </tr>
-                  </thead>
-            <tbody>
-            @foreach ($cursos as $curso)
-                <!-- Post preview-->
-                <div class="post-preview">
-                      <tr>
-                        <td>{{ $curso->title }}</td>
-                        <td>{{ $curso->description }}</td>
-                        <td>{{ $curso->date }}</td>
-                        <!-- Se pone la palabra "Link" porque las url son muy grandes y se salen de su cuadro -->
-                        <td><a href="{{ $curso->link }}"> Link </a></td>
-                        <td class="text-center">
-                            <a  href="{{ route('cursosApp.edit', $curso->id)}}" class="btn btn-primary btn-sm">Editar</a>
-                            <form action="{{ route('cursosApp.destroy', $curso->id)}}" method="post" style="display: inline-block">
-                              @csrf
-                              @method('DELETE')
-                              <button class="btn btn-danger btn-sm" type="submit">Delete</button>
-                            </form>
-                        </td>
-                </div>
-                <!-- Divider-->
-                <hr class="my-4" />
-                </td>
-                </tr>
-                @endforeach
-                <a class="btn btn-success" href="{{ route('cursosApp.create')}}"> Crear Nuevo Cursos</a>
-                </tbody>
-            </table>
+<div class="container">
+  <section class="container mt-5">
+    <table class="table table-striped" aria-label>
+      <thead>
+        <tr>
+          <th scope="col">Titulo</th>
+          <th scope="col">Descripción</th>
+          <th scope="col">Fecha</th>
+          <th scope="col">Link de imagen</th>
+          <th scope="col">Acción</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($cursos as $curso)
+        <!-- Post preview-->
+        <div class="post-preview">
+          <tr>
+            <td>{{ $curso->title }}</td>
+            <td>{{ $curso->description }}</td>
+            <td>{{ $curso->date }}</td>
+            <!-- Se pone la palabra "Link" porque las url son muy grandes y se salen de su cuadro -->
+            <td><a href="{{ $curso->link }}"> Link </a></td>
+            <td class="text-center">
+              <div class="container-sm">
+                <a href="{{ route('cursosApp.edit', $curso->id)}}" class="btn btn-primary btn-sm">Editar</a>
+              </div>
+              
+              <br>
+              <div class="container-sm">
+                <form action="{{ route('cursosApp.destroy', $curso->id)}}" method="post" style="display: inline-block">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger btn-sm" type="submit">Borrar</button>
+                </form>
+              </div>
+            </td>
+        </div>
+        <!-- Divider-->
+        <hr class="my-4" />
+        </td>
+        </tr>
+        @endforeach
+        <br>
+        <div class="container-sm">
+          <a class="btn btn-success" href="{{ route('cursosApp.create')}}"> Crear nuevo curso</a>
+        </div>
+      </tbody>
+    </table>
+  </section>
+</div>
 @endsection
